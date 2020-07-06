354df56588bcf78383a189284795b74fc497d925
Pavan Mallik <54065955+pavan55523@users.noreply.github.com>
2020-07-06 21:41:37 +0530

    Update invisible_cloak.py

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -26,22 +26,27 @@ while(cap.isOpened()):
 	blurred = cv2.GaussianBlur(hsv, value,0)
 	
 	# Defining lower range for red color detection.
-	lower_red = np.array([10,120,70])
-	upper_red = np.array([20,255,255])
+	lower_red = np.array([0,120,70])
+	upper_red = np.array([10,255,255])
 	mask1 = cv2.inRange(hsv,lower_red,upper_red)
 	
 	# Defining upper range for red color detection
-	lower_red = np.array([160,120,70])
-	upper_red = np.array([170,255,255])
+	lower_red = np.array([170,120,70])
+	upper_red = np.array([180,255,255])
 	mask2 = cv2.inRange(hsv,lower_red,upper_red)
 	
 	# Addition of the two masks to generate the final mask.
-	mask = mask1+mask2
-	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
-	
-	# Replacing pixels corresponding to cloak with the background pixels.
-	img[np.where(mask==255)] = background[np.where(mask==255)]
+	mask1 = mask1+mask2
+	mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3),np.uint8),iterations=2)
+	mask1 = cv2.dilate(mask1,np.ones((3,3),np.uint8),iterations=1)
+	mask2 = cv2.bitwise_not(mask1)
+        
+        # Replacing pixels
+	res1 = cv2.bitwise_and(background,background,mask=mask1)
+	res2 = cv2.bitwise_and(img,img,mask=mask2)
+	img= cv2.addWeighted(res1,1,res2,1,0)
 	cv2.imshow('Display',img)
 	k = cv2.waitKey(10)
 	if k == 27:
 		break
+
